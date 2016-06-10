539befa48adbf2b3786830c60d1ee57e1908a80c
Thomas Hallock <hallock@gmail.com>
2016-06-10 00:14:22 -0400

    fixup! removed support for bundle-side access to app config

--- entry.js
+++ entry.js
@@ -1,5 +1,3 @@
-var path = require('path');
-var config = require('./runtime-config');
 var context = require.context(__PATH_TO_CARDS, true, __CARD_PATTERN);
 context.keys().map(context);
 if (module.hot) {
